edfea48547f869c0b26ddb3a18ec1f4b82b49e5a
PAYCHEX\vkundana <vkundanagurthi@paychex.com>
2020-03-24 14:02:09 -0400

    ActiveDirectory module install information

---
+++ ActiveDirectory/ActiveDirectoryModuleInformation.ps1
@@ -0,0 +1,23 @@
+
+#ActiveDirectoryModuleInformation.ps1
+## Created by Vijred on 3/24/2020
+
+# Check if ActiveDirectory is installed
+Get-Module ActiveDirectory
+
+#Install AD Module
+# --> Install  Remote Server Administration Tools
+
+# import module
+Import-Module ActiveDirectory
+
+
+# Explore commandlets in ActiveDirectory 
+get-command -module ActiveDirectory
+
+# Most commonlyt used cmdlets 
+Get-ADGroup
+Get-ADGroupMember
+Get-ADObject
+Get-ADUser
+
