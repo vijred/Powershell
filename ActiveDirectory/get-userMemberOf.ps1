e846c32809dfbd65c77ceab5a0aec4bd4b9fd854
Kundanagurthi <vkundanagurthi@paychex.com>
2021-04-21 13:18:33 -0400

    Active-directory member-of infomration

---
+++ ActiveDirectory/get-userMemberOf.ps1
@@ -0,0 +1,29 @@
+﻿
+## Other option is: net user /domain UserAlias 
+
+([string]$UserId)
+
+
+$UserId = "vijayusername"
+$groups = @()
+
+Import-Module ActiveDirectory
+
+$ui = Get-ADUser -Identity $UserId -Properties memberof 
+
+ForEach($gdn in $($ui.MemberOf))
+{
+    $groups +=  Get-ADGroup -Identity $gdn -Properties Name, GroupScope, GroupCategory, SamAccountName, mail, proxyAddresses 
+<#    $groups += Get-ADGroup -Identity $gdn -Properties proxyAddresses | Select-Object Name, GroupScope, GroupCategory, SamAccountName, mail, proxyAddresses
+    (Get-ADGroup -Identity $gdn).proxyAddresses
+    Get-ADGroup -Identity $gdn -Properties *
+    #>
+} 
+
+$groups | Select-Object Name, GroupScope, GroupCategory, SamAccountName, mail | Sort-Object Name | Format-Table
+
+
+# Get-ADGroup -Identity ($ui.MemberOf)[10] -Properties *
+# Get-ADGroup -Identity ($ui.MemberOf)[1] -Properties * 
+## SQL Way of doing it exec xp_logininfo 'Domain\vijay','all'; 
+
